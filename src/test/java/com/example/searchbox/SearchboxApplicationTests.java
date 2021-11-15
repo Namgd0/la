870e8cc388cc1f953067a1d4e69fc7236f26199d
nam <namgd69@gmail.com>
2021-11-15 09:35:36 +0700

    search box

---
+++ src/test/java/com/example/searchbox/SearchboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
